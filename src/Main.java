6ce0a1797a34df97b8870c02e17a1e3c3c4715df
Sergey <i@s-ponkin.ru>
2023-03-30 15:17:27 +0300

    Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello my");
         }
+
+        Scanner scanner = new Scanner(System.in);
     }
 }
