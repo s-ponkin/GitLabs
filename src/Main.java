15277340c1322e02c6de00d0fcee41a16347c3fe
Sergey <i@s-ponkin.ru>
2023-03-30 16:21:10 +0300

    j change

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("Всем добрый день!");
+            System.out.println("У меня отличные новости!");
         }
 
         Set<Integer> number = new HashSet<>();
