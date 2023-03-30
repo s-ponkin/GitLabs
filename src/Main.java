ea9b4a4c7845ef7f4d14738a31f77b41354cfb21
Sergey <i@s-ponkin.ru>
2023-03-30 16:22:44 +0300

    m change

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
-            System.out.println("Hello my friend");
             System.out.println("Всем добрый день!");
         }
 
