758bd3ce841202adb21563a9b363d6c0f9d2a212
Sergey <i@s-ponkin.ru>
2023-03-30 13:39:15 +0300

    + Random class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello my");
         }
+        
+        Random random = new Random();
+        random.ints(3).forEach(System.out::println);
     }
 }
