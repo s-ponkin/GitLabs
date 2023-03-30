df2932f30562aff8ea1854a4a0bb06aee872afd8
Sergey <i@s-ponkin.ru>
2023-03-30 13:47:41 +0300

    add Set

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello my");
         }
+
+        Set<Integer> number = new HashSet<>();
+        number.add(1);
+        number.add(2);
+        number.add(1);
     }
 }
